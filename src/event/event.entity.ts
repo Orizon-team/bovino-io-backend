73ef25b16aef3b9d59497dc3136dc934ec26b080
FerSoltez <19090483@alumno.utmetropolitana.edu.mx>
2025-11-11 17:39:32 -0600

    chambio event

--- src/event/event.entity.ts
+++ src/event/event.entity.ts
@@ -11,13 +11,28 @@ export class Evento {
   @PrimaryGeneratedColumn({ name: 'id_event' })
   id_event: number;
 
+  @Field(() => Int, { name: 'Codigo_Evento' })
+  get codigoEvento(): number {
+    return this.id_event;
+  }
+
   @Field({ nullable: true })
   @Column({ type: 'varchar', length: 100, nullable: true, name: 'Event_Code' })
   Event_Code?: string;
 
+  @Field({ nullable: true, name: 'Descripcion_Evento' })
+  get descripcionEvento(): string | undefined {
+    return this.Event_Description;
+  }
+
   @Field({ nullable: true })
   @Column({ type: 'varchar', length: 100, nullable: true, name: 'Event_Type' })
   Event_Type?: string;
+
+  @Field({ nullable: true, name: 'Tipo_Evento' })
+  get tipoEvento(): string | undefined {
+    return this.Event_Type;
+  }
 
   @Field({ nullable: true })
   @Column({ type: 'text', nullable: true, name: 'Event_Description' })
@@ -27,14 +42,29 @@ export class Evento {
   @Column({ type: 'time', nullable: true, name: 'time' })
   time?: string;
 
+  @Field({ nullable: true, name: 'hora' })
+  get hora(): string | undefined {
+    return this.time;
+  }
+
   @Field({ nullable: true })
   @Column({ type: 'date', nullable: true, name: 'date' })
   date?: string;
+
+  @Field({ nullable: true, name: 'fecha' })
+  get fecha(): string | undefined {
+    return this.date;
+  }
 
   @Field(() => Vaca, { nullable: true })
   @ManyToOne(() => Vaca, { nullable: true, onUpdate: 'CASCADE', onDelete: 'SET NULL' })
   @JoinColumn({ name: 'id_cow' })
   cow?: Vaca;
+
+  @Field(() => Vaca, { nullable: true, name: 'vaca' })
+  get vaca(): Vaca | undefined {
+    return this.cow;
+  }
 
   @Field(() => Tag, { nullable: true })
   @ManyToOne(() => Tag, { nullable: true, onUpdate: 'CASCADE', onDelete: 'SET NULL' })
@@ -45,4 +75,9 @@ export class Evento {
   @ManyToOne(() => DispositivoESP32, { nullable: true, onUpdate: 'CASCADE', onDelete: 'SET NULL' })
   @JoinColumn({ name: 'id_device' })
   device?: DispositivoESP32;
+
+  @Field(() => DispositivoESP32, { nullable: true, name: 'dispositivo' })
+  get dispositivo(): DispositivoESP32 | undefined {
+    return this.device;
+  }
 }
