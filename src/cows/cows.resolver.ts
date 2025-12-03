7af699e6fe381e9d171239670640b3d9f520d6fd
FerSoltez <19090483@alumno.utmetropolitana.edu.mx>
2025-12-03 14:36:33 -0600

    Prueba manual

--- src/cows/cows.resolver.ts
+++ src/cows/cows.resolver.ts
@@ -3,10 +3,11 @@ import { VacasService } from './cows.service';
 import { Vaca } from './cow.entity';
 import { CreateVacaInput } from './dto/create-cow.input';
 import { UpdateVacaInput } from './dto/update-cow.input';
+import { CowRealtimeGateway, CowRegistrationRequestPayload } from './cow-realtime.gateway';
 
 @Resolver(() => Vaca)
 export class VacasResolver {
-  constructor(private vacasService: VacasService) {}
+  constructor(private vacasService: VacasService, private cowGateway: CowRealtimeGateway) {}
 
   @Query(() => [Vaca])
   vacas() {
@@ -36,5 +37,29 @@ export class VacasResolver {
   @Mutation(() => Boolean)
   deleteVaca(@Args('id', { type: () => Int }) id: number) {
     return this.vacasService.remove(id);
+  }
+
+  @Mutation(() => Boolean, { description: 'Temporal: dispara manualmente cow.registration.request' })
+  testCowRegistration(
+    @Args('id_user', { type: () => Int }) id_user: number,
+    @Args('tag_id', { type: () => Int }) tag_id: number,
+    @Args('zone_id', { type: () => Int }) zone_id: number,
+    @Args('zone_name', { type: () => String }) zone_name: string,
+    @Args('user_name', { type: () => String, nullable: true }) user_name?: string,
+    @Args('user_email', { type: () => String, nullable: true }) user_email?: string,
+    @Args('id_tag', { type: () => String, nullable: true }) id_tag?: string,
+    @Args('mac_address', { type: () => String, nullable: true }) mac_address?: string,
+    @Args('redirect_url', { type: () => String, nullable: true }) redirect_url?: string,
+  ) {
+    const payload: CowRegistrationRequestPayload = {
+      tag_id,
+      id_tag,
+      mac_address,
+      zone: { id: zone_id, name: zone_name },
+      user: { id_user, name: user_name ?? null, email: user_email ?? null },
+      redirect_url,
+    };
+    this.cowGateway.emitRegistrationRequest(id_user, payload);
+    return true;
   }
 }
